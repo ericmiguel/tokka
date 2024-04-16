78251c0164ae0351c831b70131c89edf896bc016
ericmiguel <eric.mrib@gmail.com>
2024-04-16 11:20:20 -0300

    refactor: update sample code to better mirrors README quick usage

--- samples/sample1.py
+++ samples/sample1.py
@@ -9,55 +9,58 @@ from tokka import Collection
 from tokka import Database
 
 
-# Define a Pydantic model to represent the data
-class UserProfile(BaseModel):
+class User(BaseModel):
+    """Sample data."""
+
     name: str
     email: str
 
 
-# Create a subclass of Database to define the your collections
 class DB(Database):
+    """A tokka.Database subclass to easily accesst the your collections."""
+
     @property
-    def user_profiles(self) -> Collection:
-        return self.get_collection("profiles")
+    def users(self) -> Collection:
+        return self.get_collection("users")
 
 
 if __name__ == "__main__":
     db = DB("tokka", connection=config.mongodb.URI)
-    user1 = UserProfile(name="John Doe", email="john.doe@example.com.br")
-    user2 = UserProfile(name="Emma Su", email="ema.sue@example.com.br")
-    user3 = UserProfile(name="Joanne Doe", email="joanne.doe@example.com.br")
+    user1 = User(name="John Doe", email="john.doe@example.com.br")
+    user2 = User(name="Emma Su", email="ema.sue@example.com.br")
+    user3 = User(name="Joanne Doe", email="joanne.doe@example.com.br")
 
     async def crud() -> None:
+        """Perform CRUD some sample operations."""
         insert_results = await asyncio.gather(
-            db.user_profiles.insert_one(user1),
-            db.user_profiles.insert_one(user2),
-            db.user_profiles.insert_one(user1, exclude="name"),
+            db.users.insert_one(user1),
+            db.users.insert_one(user2),
+            db.users.insert_one(user1, exclude="name"),
         )
 
         set_results = await asyncio.gather(
-            db.user_profiles.set(user1, match="name", upsert=True, exclude="email"),
-            db.user_profiles.find_one(user2, filter_by="name"),
+            db.users.set(user1, match="name", upsert=True, exclude="email"),
+            db.users.find_one(user2, filter_by="name"),
         )
 
         find_results = await asyncio.gather(
-            db.user_profiles.find_one(user1, filter_by="name"),
-            db.user_profiles.find_one(user2, filter_by="name"),
-            db.user_profiles.find_one(user1, filter_by="email"),
+            db.users.find_one(user1, filter_by="name"),
+            db.users.find_one(user2, filter_by="name"),
+            db.users.find_one(user1, filter_by="email"),
         )
 
         find_one_and_replace_results = await asyncio.gather(
-            db.user_profiles.find_one_and_replace(user1, user2, filter_by="name"),
-            db.user_profiles.find_one_and_replace(
+            db.users.find_one_and_replace(user1, user2, filter_by="name"),
+            db.users.find_one_and_replace(
                 user2, user1, filter_by="name", return_old=True
             ),
         )
 
         find_one_and_update_results = await asyncio.gather(
-            db.user_profiles.find_one_and_update(
+            db.users.find_one_and_update(
                 user1, {"$set": {"name": "Mario"}}, filter_by="name", return_old=False
             ),
-            db.user_profiles.find_one_and_update(
+            db.users.find_one_and_update(
                 user2, {"$set": {"name": "Maria"}}, filter_by="name", return_old=True
             ),
         )
@@ -65,21 +68,21 @@ if __name__ == "__main__":
         user1.name = "Mario"
 
         find_one_and_delete_results = await asyncio.gather(
-            db.user_profiles.find_one_and_delete(user1, filter_by="name"),
-            db.user_profiles.find_one_and_delete(user2, filter_by="name"),
+            db.users.find_one_and_delete(user1, filter_by="name"),
+            db.users.find_one_and_delete(user2, filter_by="name"),
         )
 
         find_one_and_set = await asyncio.gather(
-            db.user_profiles.find_one_and_set(
+            db.users.find_one_and_set(
                 user1, exclude="email", filter_by="name", return_old=False
             ),
-            db.user_profiles.find_one_and_set(
+            db.users.find_one_and_set(
                 user2, exclude="email", filter_by="name", return_old=True
             ),
         )
 
         replace_one_results = await asyncio.gather(
-            db.user_profiles.replace_one(user2, user3, filter_by="email"),
+            db.users.replace_one(user2, user3, filter_by="email"),
         )
 
         ic(
@@ -90,12 +93,10 @@ if __name__ == "__main__":
             find_one_and_update_results,
             find_one_and_delete_results,
             find_one_and_set,
-            replace_one_results
-            
-
+            replace_one_results,
         )
 
-        await db.user_profiles.collection.delete_many({})
+        await db.users.collection.delete_many({})
 
     asyncio.run(crud())
 
