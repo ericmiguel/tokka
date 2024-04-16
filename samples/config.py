c92bf5b1ab1c5a1e57eefd90e8eb0b27189ddc87
ericmiguel <eric.mrib@gmail.com>
2024-04-16 11:20:20 -0300

    refactor: update sample code to better mirrors README quick usage

--- samples/config.py
+++ samples/config.py
@@ -1,9 +1,13 @@
+"""Config file for Tokka code samples and usa examples."""
+
 from pydantic import Field
 from pydantic_settings import BaseSettings
 from pydantic_settings import SettingsConfigDict
 
 
 class MongoDB(BaseSettings, case_sensitive=False):
+    """MongoDB settings."""
+
     model_config = SettingsConfigDict(env_file=".env.db")
     URI: str = Field(default=...)
 
