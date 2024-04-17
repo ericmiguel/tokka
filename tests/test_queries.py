4692ee0e933dfc7a9696652ee2ee1a5a6c8377a8
ericmiguel <eric.mrib@gmail.com>
2024-04-16 22:15:22 -0300

    test: find_one

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -16,3 +16,17 @@ async def test_insert_one(
             assert isinstance(result, InsertOneResult) == True
             assert ObjectId().is_valid(result.inserted_id) == True
             await session.abort_transaction()
+
+
+@pytest.mark.asyncio(scope="session")
+async def test_find_one(
+    client: Client,
+    collection: Collection,
+    user_1: BaseModel,
+) -> None:
+    async with await client.client.start_session() as session:
+        async with session.start_transaction():
+            await collection.insert_one(user_1, session=session)
+            found = await collection.find_one(user_1, hide={"_id"}, session=session)
+            assert user_1.model_dump() == found
+            await session.abort_transaction()
